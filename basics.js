f746c4793065d31a6a8fb6a3a03491ad6f7b3a95
Divyansh Vishwakarma <119093182+deevee47@users.noreply.github.com>
2024-03-25 20:07:18 +0530

    Update basics.js

--- basics.js
+++ basics.js
@@ -3,16 +3,17 @@
 404 - Not Found
 500 - Internal Server Error
 411 - Input was incorrect
-403 - Returning something you do not have the access to */
+403 - Returning something you do not have access to */
 
 //make sure to npm init -y, npm install express
 
 const express = require("express")
 const app = express() //making instance of express
 
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.json()); 
+//app.use() takes a middleware as an input and runs it in all the requests written below it.
 
-//Basically, you can send data in multiple types to a server (Eg: A POST Request), app.use(express.json()) parses the data to a readable format for the backend code/server
+//Basically, you can send data in multiple types to a server (Eg: A POST Request), (express.json() parses the data to a readable format for the backend code/server
 
 // express.json() middleware parses the JSON data and makes it available in req.body property of your route handlers.
 
